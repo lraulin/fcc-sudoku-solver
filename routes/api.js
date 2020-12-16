169484d2f331530dd4cfaea06b28c42f70085e64
Lee Raulin <leeraulin@gmail.com>
2020-12-16 18:14:35 -0500

    ...

--- routes/api.js
+++ routes/api.js
@@ -2,16 +2,48 @@
 
 const solver = require("../controllers/sudoku-solver.js");
 
+const OK = 200;
+const BAD_REQUEST = 400;
+
+const validatePuzzle = (res, puzzle) => {
+  if (!puzzle) {
+    res.status(BAD_REQUEST).send({ error: "Required field missing" });
+    return false;
+  }
+  if (puzzle.length !== 81) {
+    res
+      .status(BAD_REQUEST)
+      .send({ error: "Expected puzzle to be 81 characters long" });
+    return false;
+  }
+  if (!/^[1-9.]{81}$/.test(puzzle)) {
+    res.status(BAD_REQUEST).send({ error: "Invalid characters in puzzle" });
+    return false;
+  }
+  return true;
+};
+
 module.exports = function (app) {
   app.route("/api/check").post((req, res) => {
     const { puzzle, coordinate, value } = req.body;
+    // validation
+    const isValid = validatePuzzle(res, puzzle);
+    if (!isValid) return;
     const valid = solver.check(puzzle, coordinate, value);
     res.send({ valid });
   });
 
   app.route("/api/solve").post((req, res) => {
     const { puzzle } = req.body;
+    // validation
+    const isValid = validatePuzzle(res, puzzle);
+    if (!isValid) return;
+
     const solution = solver.solve(puzzle);
-    res.send({ solution });
+    if (solution) {
+      return res.status(OK).send({ solution });
+    } else {
+      return res.status(BAD_REQUEST).send({ error: "Puzzle cannot be solved" });
+    }
   });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,7 +7,7 @@ chai.use(chaiHttp);
 
 describe("Functional Tests", () => {
   describe("POST /api/solve", () => {
-    it("it should solve the puzzle", (done) => {
+    it("should return solution when given solvable puzzle", (done) => {
       const puzzle =
         "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
       chai
@@ -20,6 +20,51 @@ describe("Functional Tests", () => {
             "solution",
             "135762984946381257728459613694517832812936745357824196473298561581673429269145378"
           );
+          done();
+        });
+    });
+
+    it("should return error when given not given puzzle", (done) => {
+      chai
+        .request(server)
+        .post("/api/solve")
+        .send()
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res.body).to.have.property("error", "Required field missing");
+          done();
+        });
+    });
+
+    it("should return error when puzzle length is not 81", (done) => {
+      const puzzle =
+        "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.54258";
+
+      chai
+        .request(server)
+        .post("/api/solve")
+        .send({ puzzle })
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res.body).to.have.property(
+            "error",
+            "Expected puzzle to be 81 characters long"
+          );
+          done();
+        });
+    });
+
+    it("should return error when puzzle is not solvable", (done) => {
+      const puzzle =
+        "..9..5.1.85.4....2432.9.6..1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
+
+      chai
+        .request(server)
+        .post("/api/solve")
+        .send({ puzzle })
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res.body).to.have.property("error", "Puzzle cannot be solved");
           done();
         });
     });
